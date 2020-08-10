85a6baa7fd5ba6f83f499ac3dc3a148a5a1e234d
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2020-08-10 00:12:20 -0400

    basic product page

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
-var product = 'Longaniza de Brocoli'
+var app = new Vue({
+  el: '#app',
+  data: {
+    img: './assets/printed-book.jpg',
+    product: 'Longaniza de Brocolli',
+    description: 'Viscosas pero sabrosas',
+    link: './printed_book.html',
+    stock: 100,
+    onSale: true,
+    details: ['Tapa dura', '487 páginas', '458gr'],
+    variants: [
+      {
+        variantId: 2234,
+        variantType: 'printed',
+        img: './assets/printed-book.jpg'
+      },
+      {
+        variantId: 2235,
+        variantType: 'pdf',
+        img: './assets/pdf-book.jpg'
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1
+      }
+    },
+    updateImg(img) {
+      this.img = img
+    }
+  },
+  computed: {
+    inStock() {
+      return this.stock > 0 ? true : false
+    }
+  }
+
+})
